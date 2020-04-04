20c74a57fa6acc8422fc3fbfb37c5fb4a3284d60
timwea <timwea@corp.symetra.com>
2020-04-03 21:41:55 -0700

    day one

---
+++ thirtydaychallenge/1.go
@@ -0,0 +1,18 @@
+package main
+
+import "sort"
+
+// https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3283/
+
+func singleNumber(nums []int) int {
+	sort.Ints(nums)
+	l := len(nums)
+	for i := 0; i < l; i += 2 {
+		if (i == l-1) || (nums[i] != nums[i+1]) {
+			return nums[i]
+		}
+
+	}
+	return 0
+
+}
