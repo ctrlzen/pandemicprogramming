cc6c599c7fb604017fa61b8cb5f103536fb5f152
ctrlzen <timwea@corp.symetra.com>
2020-04-10 11:53:53 -0700

    fixed link

--- thirtydaychallenge/4.go
+++ thirtydaychallenge/4.go
@@ -1,6 +1,6 @@
 package main
 
-// https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/
+// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3286/
 
 func moveZeroes(nums []int) {
 
